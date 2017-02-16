41da0ec27d99493c19019e0658febfd68c065329
Lion <439635374@qq.com>
2017-02-16 15:05:15 +0800

    集成Activiti
    Signed-off-by: Lion <439635374@qq.com>

---
+++ src/com/lion/sys/mvc/workflow/WorkFlowController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.lion.sys.mvc.workflow;
+
+import com.lion.sys.mvc.base.BaseController;
+
+/**
+ * @author Lion
+ * @date 2017年2月16日 下午4:04:25
+ * @qq 439635374
+ */
+/**
+ * @author Administrator
+ *
+ */
+public class WorkFlowController extends BaseController{
+	/***
+	 * 获取流程在线编辑器页面
+	 */
+}
